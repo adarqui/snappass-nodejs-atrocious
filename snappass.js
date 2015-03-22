028139269e90ec1bf60bf99b15f0e13d0e71313c
Andrew Darqui <andrew.darqui@gmail.com>
2015-03-22 15:23:11 -0400

    some tests, slight refactor

--- snappass.js
+++ snappass.js
@@ -13,15 +13,14 @@ var
 var
     snap_config = require('./lib/config.js'),
     snap_time = require('./lib/time.js'),
-    snap_proc = require('./lib/proc.js'),
-    snap_ops = require('./lib/ops.js')
+    snap_proc = require('./lib/proc.js')
     ;
 
 // Main entry point into snappass.
 // Builds the config, connects to redis, and sets up the express web server.
-var init = function(path, cb) {
-    // Build the default config.
-    var config = snap_config.defaultConfig();
+var SnapPass = function() {
+
+    var self = this;
 
     // state holds redis & express objects
     var state = {};
@@ -29,96 +28,139 @@ var init = function(path, cb) {
     // snap holds the merged config
     var snap = {};
 
-    // acb = async cb
-    async.series([
-        function(acb) {
-            fs.readFile(path, 'utf-8', function configurationReadAttempt(err, data) {
-                snap_proc.exitIfTrue(err, 1);
-                var js = JSON.parse(data);
-                // Overwrite any specific fields specified in the user supplied config, via merge.
-                snap = merge.recursive(true, config, js);
-                acb(null, {});
-            });
-        },
-        function(acb) {
-            redis_connect(state, snap, function redisConnectionAttempt(err, data) {
-                snap_proc.exitIfTrue(err, 1);
-                acb(null, {});
-            });
-        },
-        function(acb) {
-            express_server(state, snap, function expressServerAttempt(err, data) {
-                snap_proc.exitIfTrue(err, 1);
-                acb(null, {});
-            });
-        },
-    ],  function(err, results) {
-            snap_proc.exitIfTrue(err, 1);
-            cb(null, snap);
-    });
-    
-};
-
-// Connect to redis.
-// Optionally (via config), connect to a specific database and/or authenticate using 'auth'.
-var redis_connect = function(state, snap, cb) {
-    state.redis = redis.createClient();
-    state.redis.select(snap.redis.db, function redisSelectResult(err, data) {
-        snap_proc.exitIfTrue(err, 1);
-        if (snap.redis.auth.length > 0) {
-            state.redis.auth(snap.redis.auth, function(err, data) {
-                snap_proc.exitIfTrue(err, 1);
-            });
-        }
-        cb(null, snap);
-    });
-};
-
-// Setup the express server which will handle the GET/POST reqs and our static content.
-var express_server = function(state, snap, cb) {
-    state.web = express();
-    // By default we shouldn't be logging.
-    if (snap.web.logging === true) {
-        state.web.use(morgan('combined'));
+    this.getSnap = function() {
+        return snap;
     }
-    state.web.use('/static', express.static(snap.web.static));
-    state.web.get('/', function indexRequest(req, res) {
-        res.status(200).sendFile(__dirname + '/static/index.html');
-    });
-    // Attempt to GET a key.
-    // If the key exists, return it and delete it.
-    state.web.get('/:key', function keyRequest(req, res) {
-        snap_ops.get_password(state, req.params.key, function(err, password) {
-            if (err == null) {
-                res.status(200).send(password);
-            } else {
-                res.status(400).send();
-            }
+
+    this.init = function(path, cb) {
+        // Build the default config.
+        var config = snap_config.defaultConfig();
+
+        // acb = async cb
+        async.series([
+            function(acb) {
+                fs.readFile(path, 'utf-8', function configurationReadAttempt(err, data) {
+                    var js = JSON.parse(data);
+                    // Overwrite any specific fields specified in the user supplied config, via merge.
+                    snap = merge.recursive(true, config, js);
+                    acb(err, {});
+                });
+            },
+            function(acb) {
+                self.redis_connect(function redisConnectionAttempt(err, data) {
+                    acb(err, {});
+                });
+            },
+            function(acb) {
+                self.express_server(function expressServerAttempt(err, data) {
+                    acb(err, {});
+                });
+            },
+        ],  function(err, results) {
+                cb(err);
         });
-    });
-    
-    // Post a password and ttl expiration (day, week, month).
-    // If successful, returns a v4 uuid.
-    // This v4 uuid can be used to retrieve the original password stored in redis.
-    // If ttl seconds pass, the password stored in redis will expire & disappear.
-    state.web.post('/:password/:ttl', function passwordPostRequest(req, res) {
-        var password = req.params.password;
-        var ttl = snap_time.convert(req.params.ttl);
-        if (ttl === 0) {
-            return res.status(400).send();
+        
+    };
+
+    // Close the express & Redis servers
+    this.fini = function() {
+        if (state.web !== undefined && state.web.server !== undefined) {
+            state.web.server.close();
         }
-        snap_ops.set_password(state, password, ttl, function setPasswordStatus(err, key) {
-            if (err == null) {
-                res.status(200).send(key);
-            } else {
-                res.status(400).send();
+        if (state.redis !== undefined) {
+            state.redis.end();
+        }
+    }
+
+    // Connect to redis.
+    // Optionally (via config), connect to a specific database and/or authenticate using 'auth'.
+    this.redis_connect = function(cb) {
+        state.redis = redis.createClient();
+        state.redis.select(snap.redis.db, function redisSelectResult(err, data) {
+            snap_proc.exitIfTrue(err, 1);
+            if (snap.redis.auth.length > 0) {
+                state.redis.auth(snap.redis.auth, function(err, data) {
+                    snap_proc.exitIfTrue(err, 1);
+                });
             }
+            cb(null);
         });
-    });
-    state.web.server = state.web.listen(snap.web.port);
-    cb(null, snap);
+    };
+
+    // Setup the express server which will handle the GET/POST reqs and our static content.
+    this.express_server = function(cb) {
+        state.web = express();
+        // By default we shouldn't be logging.
+        if (snap.web.logging === true) {
+            state.web.use(morgan('combined'));
+        }
+        state.web.use('/static', express.static(snap.web.static));
+        state.web.get('/', function indexRequest(req, res) {
+            res.status(200).sendFile(__dirname + '/static/index.html');
+        });
+        // Attempt to GET a key.
+        // If the key exists, return it and delete it.
+        state.web.get('/:key', function keyRequest(req, res) {
+            self.get_password(req.params.key, function(err, password) {
+                if (err == null && password != null) {
+                    res.status(200).send(password);
+                } else {
+                    res.status(400).send();
+                }
+            });
+        });
+        
+        // Post a password and ttl expiration (day, week, month).
+        // If successful, returns a v4 uuid.
+        // This v4 uuid can be used to retrieve the original password stored in redis.
+        // If ttl seconds pass, the password stored in redis will expire & disappear.
+        state.web.post('/:password/:ttl', function passwordPostRequest(req, res) {
+            var password = req.params.password;
+            var ttl = snap_time.convert(req.params.ttl);
+            if (ttl === 0) {
+                return res.status(400).send();
+            }
+            self.set_password(password, ttl, function setPasswordStatus(err, key) {
+                if (err == null) {
+                    res.status(200).send(key);
+                } else {
+                    res.status(400).send();
+                }
+            });
+        });
+        state.web.server = state.web.listen(snap.web.port);
+        cb(null, snap);
+    };
+
+    // wrap reddis keys like so: snap:<key>
+    this.wrap_key = function(key) {
+        return ('snap:'+key);
+    };
+
+    // Generates a key (uuid), sets the password for this key, then sets the expiration ttl on the key.
+    this.set_password = function(password, ttl, cb) {
+        var key = uuid.v4();
+        state.redis.set(self.wrap_key(key), password, function redisSetKeyAndPassword(err, data) {
+            snap_proc.exitIfTrue(err, 1);
+            cb(err, key);
+        });
+        state.redis.expire(self.wrap_key(key), ttl, function(err,data) {});
+    };
+
+    // attempts to get the password stored for a given key
+    // if the key is found, delete it from redis.
+    this.get_password = function(key, cb) {
+        state.redis.get(self.wrap_key(key), function redisGetPasswordFromKey(err, password) {
+            if (err == null) {
+                state.redis.del(self.wrap_key(key), function(err,data) {});
+            }
+            cb(err, password);
+        });
+    };
+
+    return this;
 };
 
 module.exports = {
-    init: init
+    SnapPass: SnapPass
 };
--- test/vows.js
+++ test/vows.js
@@ -1,15 +1,51 @@
 var
+    snap = require('../snappass'),
     vows = require('vows'),
     assert = require('assert')
     ;
 
+var app;
+
+// for debugging.
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err.stack);
+});
+
 vows.describe('Tests').addBatch({
-    'Test' : {
-        topic: function(){
-            return 1;
+    'Setup': {
+        topic: function() {
+            app = new snap.SnapPass();
+            return app;
         },
-        'should be 1': function(doc){
-            assert.equal(1, doc);
+        'should be an instance of SnapPass': function(result) {
+            assert.instanceOf(result, snap.SnapPass);
+        },
+    },
+    // eh this is wrecked..
+    'Init': {
+        topic: function() {
+            app.init('etc/config.json', this.callback);
+        },
+        'after init': {
+            topic: function(err) {
+                assert.equal(err, null);
+                this.callback(err);
+            },
+            'error should be null': function(err) {
+                assert.equal(err, null);
+            },
+            'fini': function(err) {
+                assert.equal(err, null);
+                app.fini();
+            }
         }
-    }
+    },
+    'WrapKey' : {
+        topic: function() {
+            return app.wrap_key('hello');
+        },
+        'should be snap:hello': function(key){
+            assert.equal(key, 'snap:hello');
+        }
+    },
 }).export(module);
